825b919c3af1c3fbecf27aeabd21983acbbc3de2
nanoouo <mezliniamen33@gmail.com>
2025-10-25 12:42:23 +0200

    title icon update

---
+++ app/head.js
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Out of the Box Design Studio</title>
+      <meta name="description" content="Design d’intérieur, UX/UI et architecture" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      
+      
+    </>
+  );
+}
